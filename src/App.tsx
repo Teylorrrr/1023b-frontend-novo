43e424a6cd72aedb4fa8091a7b457fa5cece9263
tyler the coder <teylor.aquino@estudante.ifms.edu.br>
2025-09-15 08:27:03 -0400

    dia15/09

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-
+import { useEffect, useState } from 'react'
+type produtoType = {
+  _id: string;
+  nome: string;
+  preco: number;
+  descricao: string;
+  urlfoto: string;
+}
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const [produtos, setProdutos] = useState<produtoType[]>([])
+useEffect(() => {
+  fetch('/api/produtos')
+    .then(response => response.json())
+    .then(data => setProdutos(data))
+    .catch(error => console.error('Error fetching data:', error));
+}, [])
+return(
+  <>
+  <div>TERERERERERERERERREEREREERRERERERERERRER</div>
+  {
+    produtos.map(produto => (
+      <div key={produto._id}>
+        <h2>{produto.nome}</h2>
+        <p>Preço: {produto.preco}</p>
+        <p>Descrição: {produto.descricao}</p>
+        <img src={produto.urlfoto} alt={produto.nome} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    ))
+  }
+  </>
+)
 }
 
 export default App
